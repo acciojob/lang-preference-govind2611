5b6f96112768c53353b4a0863ece4e087a660208
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-09-26 08:49:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+ function saveLanguagePreference() {
+            const languageSelect = document.getElementById('language');
+            const selectedLanguage = languageSelect.value;
+
+            // Store the selected language in Session Storage
+            sessionStorage.setItem('userLanguage', selectedLanguage);
+
+            alert('Language preference saved!');
+        }
+
+        // Check if there is a stored language preference on page load
+        window.onload = function () {
+            const storedLanguage = sessionStorage.getItem('userLanguage');
+            if (storedLanguage) {
+                const languageSelect = document.getElementById('language');
+                languageSelect.value = storedLanguage;
+            }
+        };
